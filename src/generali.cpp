4a15f7a5a99ef0fe1febda9fb02916afcf5496c5
Michele Scala <Michele Scala>
2015-05-02 19:04:52 +0200

    40/100 punteggio, Prima legge sistemata.. ma non riesce a funzionare la 2 legge per grafi ciclici ( non tutti )

--- src/generali.cpp
+++ src/generali.cpp
@@ -25,10 +25,13 @@ typedef std::unordered_set< int > SetGenerali;
 struct GeneraleItem{ //Generale contenuto all'interno dell'Array
 
 	int colore = NOT_VISITED;
-	SetGenerali generali;
+	SetGenerali sconfitto;
 
 	int comandato = NONE;
 	SetGenerali sottoposti;
+
+	//Generali che mi disprezzano
+	SetGenerali disprezzo;
 
 };
 
@@ -57,7 +60,8 @@ int main(int argc, char* argv[]){
 	int vinc,perd;
 	for(int I = 0; I < E; I++){
 		fscanf(input,"%d %d",&vinc,&perd);
-		grafo[perd].generali.insert(vinc);
+		grafo[perd].sconfitto.insert(vinc);
+		grafo[perd].disprezzo.insert(vinc);
 	}
 
 	fclose(input);
@@ -66,21 +70,22 @@ int main(int argc, char* argv[]){
 
 	for(int i= 0; i<V; i++) {
 		std::queue<int> to_control;
+
 		/*
 		* Scorro tutti i generali che mi hanno sconfitto
 		*/		
-		for(int g: grafo[i].generali) {
+		for(int g: grafo[i].disprezzo) {
 			/*
 			* Scorro tutti i generali che hanno sconfitto g
 			*/
-			for(int gen: grafo[g].generali){
+			for(int gen: grafo[g].disprezzo){
 				if(gen == i)
 					continue;
 				/*
 				* Se per caso non è già un generale che mi ha sconfitto allora lo aggiungo tra chi mi disprezza 
 				*/
-				if(grafo[i].generali.find(gen) == grafo[i].generali.end()){
-					grafo[i].generali.insert(gen);
+				if(grafo[i].disprezzo.find(gen) == grafo[i].disprezzo.end()){
+					grafo[i].disprezzo.insert(gen);
 					/*
 					* Elemento è colorato? 
 					* SI vuol dire che non dobbiamo andare a controllare gli elementi che contiene
@@ -96,12 +101,12 @@ int main(int argc, char* argv[]){
 		while (!to_control.empty()){
 			int g = to_control.front();
 
-			for(int gen: grafo[g].generali){
+			for(int gen: grafo[g].disprezzo){
 				if(gen == i)
 					continue;
 				
-				if(grafo[i].generali.find(gen) == grafo[i].generali.end()){
-					grafo[i].generali.insert(gen);
+				if(grafo[i].disprezzo.find(gen) == grafo[i].disprezzo.end()){
+					grafo[i].disprezzo.insert(gen);
 
 					if(grafo[g].colore != 1){
 						to_control.push(gen);
@@ -116,7 +121,8 @@ int main(int argc, char* argv[]){
 	
 	//Costruzione dell'albero
 
-	std::stringstream ss;
+	std::stringstream ssCons;
+	std::stringstream ssSottoposti;
 	int consiglieri = 0;	
 
 	for(int i=0; i<V; i++) {
@@ -126,55 +132,47 @@ int main(int argc, char* argv[]){
 		checkSottoposto(i,grafo);
 
 		if(grafo[i].comandato == -1){
-			ss << i << " ";
+			ssCons << i << " ";
 			consiglieri++;
+		}else{
+			ssSottoposti << grafo[i].comandato << " " << i << "\n";
 		}
 	}
 
 	FILE* output = fopen("output.txt","w");
 
 	fprintf(output,"%d\n",consiglieri);
-	fprintf(output,"%s\n",ss.str().c_str());
-
-	for(int i = 0; i<V; i++){
-		if(grafo[i].comandato != ROOT)
-			fprintf(output,"%d %d\n",grafo[i].comandato,i);
-	}
-
-	fclose(output);
+	fprintf(output,"%s\n",ssCons.str().c_str());
+	fprintf(output,"%s\n",ssSottoposti.str().c_str());
 	
-	//STAMPA DEL RISULTATO
-	for(int i = 0; i < V ; i++){
-		std::cout 	<< "Generale: " << i 
-					<< " <- " << grafo[i].comandato << " : ";
-		for(int g: grafo[i].generali) {
-			std::cout << g << ",";
-		}
-		std::cout << "\n";
-	}
+	fclose(output);
 
 	return 0;
 }
 
 bool checkRivalita(int g1, int g2, GeneraleItem* grafo) {
-	if(grafo[g1].generali.count(g2)==1 && grafo[g2].generali.count(g1)==1)
+	if(grafo[g1].disprezzo.count(g2)==1 && grafo[g2].disprezzo.count(g1)==1)
 		return true;
 	return false;
 }
 
 bool puoSottoposto(int gf,int gp,GeneraleItem* grafo){
-	if(grafo[gp].generali.count(gf) == 1) 		//Se per caso si odiano a vicenda NO!
+
+	//Se per caso si odiano a vicenda NO! SECONDA LEGGE
+	if(grafo[gp].disprezzo.count(gf) == 1) 		
 		return false;
+
 	//potrebbe essere figlio.. controllare i fratelli
-	bool ok = true;								//Se non ha fratelli OK!
+	bool ok = true;	 //Se non ha fratelli OK! //TERZA LEGGE
 	for(int g: grafo[gp].sottoposti){	
 		ok = checkRivalita(g,gf,grafo) ? false : ok;
 	}
+
 	return ok;
 }
 
 void checkSottoposto(int generale,GeneraleItem* grafo){
-	for(int g: grafo[generale].generali){
+	for(int g: grafo[generale].sconfitto){
 		if(puoSottoposto(generale,g,grafo)){
 			grafo[generale].comandato = g;
 			grafo[g].sottoposti.insert(generale);
