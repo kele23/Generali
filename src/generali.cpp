166dea78739c1beda683d090888ecfcfb498402b
Michele Scala <Michele Scala>
2015-05-02 17:46:33 +0200

    Programma funzionante, 0% valutazione.. da riguardare come mai

--- src/generali.cpp
+++ src/generali.cpp
@@ -3,61 +3,43 @@
 #include <stdlib.h>
 #include <unordered_set>
 #include <queue>
+#include <sstream>
 
-/**
-* Definizione della struttura di Generale
-*/
-struct Generale { //Generale Generico ( Contenuto all'interno di un Set )
+//Comandato Da
+#define NONE -2
+#define ROOT -1
 
-	int valore;
+//Colore
+#define NOT_VISITED 0
+#define VISITED 1
 
-	bool operator == ( const Generale& g ) const
-    {
-        return ( valore == g.valore );
-    }
-
-};
-
-/**
-* Definizione della funzione di Hash per il Set
-*/
-struct GeneraleHash
-{
-    
-    std::size_t operator () ( const Generale& g ) const
-    {
-        return g.valore;
-    }
-
-};
 
 /**
 * Definizione del Set
 */
-typedef std::unordered_set< Generale, GeneraleHash > SetGenerali;
+typedef std::unordered_set< int > SetGenerali;
 
 /**
 * Item dell'array di Generali
 */
 struct GeneraleItem{ //Generale contenuto all'interno dell'Array
 
-	int colore = 0;
+	int colore = NOT_VISITED;
 	SetGenerali generali;
 
-};
-
-
-struct Nodo{ //Un nodo nell'albero della gerarchia che costruiamo
-
-	Generale *padre;
-	SetGenerali *figli;
+	int comandato = NONE;
+	SetGenerali sottoposti;
 
 };
 
 /**
 * Ritorna vero se esiste rivalità tra g1 e g2, falso altrimenti
 */
-bool checkRivalita(Generale g1, Generale g2, GeneraleItem* grafo);
+bool checkRivalita(int g1, int g2, GeneraleItem* grafo);
+
+void checkSottoposto(int generale,GeneraleItem* grafo);
+
+bool puoSottoposto(int gf,int gp,GeneraleItem* grafo);
 
 int main(int argc, char* argv[]){
 	
@@ -75,11 +57,7 @@ int main(int argc, char* argv[]){
 	int vinc,perd;
 	for(int I = 0; I < E; I++){
 		fscanf(input,"%d %d",&vinc,&perd);
-		
-		Generale g;
-		g.valore = vinc;
-
-		grafo[perd].generali.insert(g);
+		grafo[perd].generali.insert(vinc);
 	}
 
 	fclose(input);
@@ -87,16 +65,16 @@ int main(int argc, char* argv[]){
 	//ELABORAZIONE DEL RISULTATO
 
 	for(int i= 0; i<V; i++) {
-		std::queue<Generale> to_control;
+		std::queue<int> to_control;
 		/*
 		* Scorro tutti i generali che mi hanno sconfitto
 		*/		
-		for(Generale g: grafo[i].generali) {
+		for(int g: grafo[i].generali) {
 			/*
 			* Scorro tutti i generali che hanno sconfitto g
 			*/
-			for(Generale gen: grafo[g.valore].generali){
-				if(gen.valore == i)
+			for(int gen: grafo[g].generali){
+				if(gen == i)
 					continue;
 				/*
 				* Se per caso non è già un generale che mi ha sconfitto allora lo aggiungo tra chi mi disprezza 
@@ -108,7 +86,7 @@ int main(int argc, char* argv[]){
 					* SI vuol dire che non dobbiamo andare a controllare gli elementi che contiene
 					* NO vuol dire che dobbiamo andare a controllare gli elementi che contiene
 					*/
-					if(grafo[g.valore].colore != 1){
+					if(grafo[g].colore != 1){
 						to_control.push(gen);
 					}
 				}
@@ -116,24 +94,16 @@ int main(int argc, char* argv[]){
 		}
 
 		while (!to_control.empty()){
-			Generale g = to_control.front();
-			/*
-			* Scorro tutti i generali che hanno sconfitto g
-			*/
-			for(Generale gen: grafo[g.valore].generali){
-				if(gen.valore == i)
+			int g = to_control.front();
+
+			for(int gen: grafo[g].generali){
+				if(gen == i)
 					continue;
-				/*
-				* Se per caso non è già un generale che mi ha sconfitto allora lo aggiungo tra chi mi disprezza 
-				*/
+				
 				if(grafo[i].generali.find(gen) == grafo[i].generali.end()){
 					grafo[i].generali.insert(gen);
-					/*
-					* Elemento è colorato? 
-					* SI vuol dire che non dobbiamo andare a controllare gli elementi che contiene
-					* NO vuol dire che dobbiamo andare a controllare gli elementi che contiene
-					*/
-					if(grafo[g.valore].colore != 1){
+
+					if(grafo[g].colore != 1){
 						to_control.push(gen);
 					}
 				}
@@ -145,24 +115,40 @@ int main(int argc, char* argv[]){
 	}
 	
 	//Costruzione dell'albero
-	Nodo root;
-	root.padre = NULL;
-	root.figli = new SetGenerali();
-	
-	Generale g;
+
+	std::stringstream ss;
+	int consiglieri = 0;	
+
 	for(int i=0; i<V; i++) {
-		if(grafo[i].generali.empty())
-			//sicuro consigliere!
-			g.valore = i;
-			g.colore = 0; //forse si potrebbe mettere già inizializzato così di default?
-			root.figli->insert(g);
+		//L'elemento è già stato aggiunto nell'albero?
+		if(grafo[i].comandato != NONE)
+			continue;
+		checkSottoposto(i,grafo);
+
+		if(grafo[i].comandato == -1){
+			ss << i << " ";
+			consiglieri++;
+		}
 	}
+
+	FILE* output = fopen("output.txt","w");
+
+	fprintf(output,"%d\n",consiglieri);
+	fprintf(output,"%s\n",ss.str().c_str());
+
+	for(int i = 0; i<V; i++){
+		if(grafo[i].comandato != ROOT)
+			fprintf(output,"%d %d\n",grafo[i].comandato,i);
+	}
+
+	fclose(output);
 	
 	//STAMPA DEL RISULTATO
 	for(int i = 0; i < V ; i++){
-		std::cout << "Generale: " << i << " -> ";
-		for(Generale g: grafo[i].generali) {
-			std::cout << g.valore << ",";
+		std::cout 	<< "Generale: " << i 
+					<< " <- " << grafo[i].comandato << " : ";
+		for(int g: grafo[i].generali) {
+			std::cout << g << ",";
 		}
 		std::cout << "\n";
 	}
@@ -170,9 +156,32 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
-bool checkRivalita(Generale g1, Generale g2, GeneraleItem* grafo) {
-	if(grafo[g1.valore].generali.count(g2)==1 && grafo[g2.valore].generali.count(g1)==1)
+bool checkRivalita(int g1, int g2, GeneraleItem* grafo) {
+	if(grafo[g1].generali.count(g2)==1 && grafo[g2].generali.count(g1)==1)
 		return true;
 	return false;
+}
+
+bool puoSottoposto(int gf,int gp,GeneraleItem* grafo){
+	if(grafo[gp].generali.count(gf) == 1) 		//Se per caso si odiano a vicenda NO!
+		return false;
+	//potrebbe essere figlio.. controllare i fratelli
+	bool ok = true;								//Se non ha fratelli OK!
+	for(int g: grafo[gp].sottoposti){	
+		ok = checkRivalita(g,gf,grafo) ? false : ok;
+	}
+	return ok;
+}
+
+void checkSottoposto(int generale,GeneraleItem* grafo){
+	for(int g: grafo[generale].generali){
+		if(puoSottoposto(generale,g,grafo)){
+			grafo[generale].comandato = g;
+			grafo[g].sottoposti.insert(generale);
+			return;
+		}
+	}
+	grafo[generale].comandato = ROOT;
+	return;
 }
 
