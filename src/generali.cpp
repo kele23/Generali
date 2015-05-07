a4d9f415d85de01b47c52b12961dd5244d86537b
Michele Scala <Michele Scala>
2015-05-07 14:56:34 +0200

    non funziona...

--- src/generali.cpp
+++ src/generali.cpp
@@ -11,7 +11,6 @@
 #include <ctime>
 
 //Comandato Da
-#define NONE -2
 #define ROOT -1
 
 //Colore BFS1
@@ -24,28 +23,30 @@
 /**
 * Definizione del Set
 */
-typedef std::unordered_set< int > SetGenerali;
+typedef std::unordered_set< int > SetNodi;
 
 /**
 * Item dell'array di Generali
 */
-struct GeneraleItem{ //Generale contenuto all'interno dell'Array
-	int colore = WHITE;
-	SetGenerali figli;
-	int gradoE;
-	SetGenerali sottoposti;
+struct Generale{ //Generale contenuto all'interno dell'Array
+	int indice = -1;
+	int min_dist;
+	SetNodi predecessori;
+	SetNodi successori;
+	SetNodi sottoposti;
+	bool onStack = false;
+
+	int comandato = ROOT;
 };
 
-std::vector<SetGenerali> cicli;
+std::vector<SetNodi> scc;
+int indice = 0;
+std::stack<int> s;
+Generale* grafo;
 
-void visita(int v, GeneraleItem* grafo, std::stack<int> path);
-
-bool ha1elementoComune(SetGenerali a, SetGenerali b);
-
-bool areInCicle(int a, int b);
-
-void merge(SetGenerali a, SetGenerali b);
-
+void tarjan(int nodo);
+bool nodoGiaPresente(int nodo, std::stack<int> st);
+bool sameScc(int i, int j);
 	
 int main(int argc, char* argv[]){
 
@@ -53,11 +54,6 @@ int main(int argc, char* argv[]){
 	
 	int V = 0;
 	int E = 0;
-	GeneraleItem* grafo;
-	SetGenerali consiglieri;
-	
-
-
 
 	//LETTURA DEL GRAFO
 
@@ -65,13 +61,13 @@ int main(int argc, char* argv[]){
 	FILE *input = fopen("input.txt","r");
 	
 	fscanf(input,"%d %d",&V,&E);
-	grafo = new GeneraleItem[V];
+	grafo = new Generale[V];
 
 	int vinc,perd;
 	for(int I = 0; I < E; I++){
 		fscanf(input,"%d %d",&vinc,&perd);
-		grafo[vinc].figli.insert(perd);
-		grafo[perd].gradoE++;
+		grafo[perd].predecessori.insert(vinc);
+		grafo[vinc].successori.insert(perd);
 	}
 
 	fclose(input);
@@ -83,75 +79,66 @@ int main(int argc, char* argv[]){
 	//ELABORAZIONE DEL RISULTATO
 	start = std::chrono::system_clock::now();
 	
-	// Chi non ha archi entranti è consigliere
-	for(int i=0; i<V; i++){
-		if(grafo[i].gradoE == 0)
-			consiglieri.insert(i);					
-	}
+	//Identificazione SCC
+	for(int i=0; i<V; i++)
+		if(grafo[i].indice == -1)
+			tarjan(i);
 	
 	end = std::chrono::system_clock::now();
 	elapsed_seconds = end-start;
 
-	std::cout << "BFS: " << elapsed_seconds.count() << std::endl;
+	std::cout << "BFS: " << elapsed_seconds.count() << std::endl;	
 
-	/*for(int i=0; i<V; i++){
-		std::cout << i << " -> ";
-		for(int g: grafo[i].disprezzo){
-			std::cout << g << " ";
-		}
-		std::cout << std::endl;
-	}*/
-
-	for(int v: consiglieri){
-		std::stack<int> path;
-		
-		grafo[v].colore = GREEN; //marco il consigliere iniziale
-		visita(v, grafo, path);
-		
-		for(int i=0; i<V; i++){
-			if(grafo[i].gradoE == 0)
-				consiglieri.insert(i);					
-		}
-	}
-
-	
 	//Costruzione dell'albero
 	start = std::chrono::system_clock::now();
+	std::stringstream ssCons;
+	std::stringstream ssSottoposti;
+	int numConsiglieri = 0;	
 
-
-	int numConsiglieri = consiglieri.size();	
-
-	/*for(int i=0; i<V; i++) {
-		//L'elemento è già stato aggiunto nell'albero?
-		if(grafo[i].comandato == NONE){
-			checkSottoposto(i,grafo);
-		}
-
-		if(grafo[i].comandato == -1){
-			ssCons << i << " ";
-			consiglieri++;
-		}else{
-			ssSottoposti << grafo[i].comandato << " " << i << "\n";
+	for(int i=0; i<V; i++) {
+		for(int s: grafo[i].successori){
+			if(!sameScc(i,s)){
+				bool ok = true;
+				for(int f: grafo[i].sottoposti)
+					if(sameScc(f,s))
+						ok = false;
+				if(ok){
+					grafo[i].sottoposti.insert(s);
+					grafo[s].comandato = i;
+					ssSottoposti << i << " " << s << std::endl;
+				}
+			}
 		}
 	}
 	end = std::chrono::system_clock::now();
 	elapsed_seconds = end-start;
-
 	std::cout << "Albero: " << elapsed_seconds.count() << std::endl;
-*/
+
+
+	int consiglieri = 0;
+	for(int i=0; i<V; i++) {
+		
+		if(grafo[i].comandato == ROOT){
+			consiglieri++;
+			ssCons << i << " ";
+		}
+
+		//Stampa sottoposti
+		/*std::cout << i << ": " << grafo[i].comandato <<" -> ";
+		for(int g: grafo[i].sottoposti){
+			std::cout << g << " ";
+		}
+		std::cout << std::endl;*/
+	}
+
+
+
 	start = std::chrono::system_clock::now();
 	FILE* output = fopen("output.txt","w");
 
-	fprintf(output,"%d\n",numConsiglieri);
-	for(int i : consiglieri){
-		fprintf(output,"%d ",i);
-	}
-	fprintf(output,"\n");
-	for(int i = 0; i < V; i++){
-		for(int s: grafo[i].sottoposti){
-			fprintf(output,"%d %d\n",i,s);
-		}
-	}
+	fprintf(output,"%d\n",consiglieri);
+	fprintf(output,"%s\n",ssCons.str().c_str());
+	fprintf(output,"%s\n",ssSottoposti.str().c_str());
 	
 	fclose(output);
 	end = std::chrono::system_clock::now();
@@ -163,87 +150,42 @@ int main(int argc, char* argv[]){
 }
 
 
-void visita(int v, GeneraleItem* grafo, std::stack<int> path) {
-
-	if(grafo[v].colore != GREEN)
-		return;
-
-	for(int f: grafo[v].figli) {
-		if(grafo[f].colore == BLACK) continue;
-		if(grafo[f].colore == GREEN) { // c'è un ciclo!
-			int q;
-			int p=v;
-			SetGenerali cicloNuovo;
-			cicloNuovo.insert(v);
-			do {
-				q=path.top();
-				path.pop();
-				cicloNuovo.insert(q);
-				grafo[p].colore=WHITE;
-				grafo[q].sottoposti.erase(p);
-				for(int figlio:grafo[p].figli){
-					grafo[figlio].gradoE++;	
-				}
-				p=q;
-			}
-			while(q!=f);
-			
-			for(SetGenerali c: cicli){
-				if (ha1elementoComune(c, cicloNuovo))
-					merge(c, cicloNuovo);
-				else cicli.push_back(cicloNuovo);
-			}
+void tarjan(int nodo) {
+	grafo[nodo].indice = indice;
+	grafo[nodo].min_dist = indice;
+	indice++;
+	s.push(nodo);
+	grafo[nodo].onStack = true;
+	int tmp;
+	for(int succ: grafo[nodo].successori) {
+		if(grafo[succ].indice == -1) {
+			tarjan(succ);
+			grafo[nodo].min_dist = std::min(grafo[nodo].min_dist, grafo[succ].min_dist);
 		}
-		if(grafo[f].colore == WHITE) { //può essere mio figlio
-			// controllo se non è in un ciclo con v
-			// controllo se non è in un ciclo con gli altri figli già assegnati di v --TERZA LEGGE
-			bool canBeS=true;
-			
-			if(areInCicle(f,v)) 
-				continue;
-			for(int s:grafo[v].sottoposti)
-				if(areInCicle(s,f)) {
-					canBeS=false; 
-					break;
-				}
-			
-			if(canBeS) {
-				grafo[f].colore = GREEN;
-				grafo[v].sottoposti.insert(f);
-				
-				for(int figlio:grafo[f].figli){
-					grafo[figlio].gradoE--;	
-				}
-				
-				path.push(v);
-				visita(f, grafo, path);
-			}
-		}
+		else if(grafo[nodo].onStack) 
+			grafo[nodo].min_dist = std::min(grafo[nodo].min_dist, grafo[succ].indice);
 	}
-	grafo[v].colore = BLACK;	
+	if(grafo[nodo].min_dist == grafo[nodo].indice) {
+		SetNodi setN;
+		
+		do {
+			tmp = s.top();
+			grafo[tmp].onStack = false;
+			setN.insert(tmp);
+			s.pop();
+		} while (tmp != nodo);
+
+		scc.push_back(setN);
+	}
 }
 
+bool sameScc(int i, int j){
 
-
-bool ha1elementoComune(SetGenerali a, SetGenerali b){
-	for(int el: a)
-		if(b.find(el) != b.end())
-			return true;		
-	return false;
-}
-
-bool areInCicle(int a, int b) {
-	for(SetGenerali set:cicli)
-		if(set.find(a) != set.end() && set.find(b) != set.end())
+	for(SetNodi set: scc){
+		if(set.count(i) == 1 && set.count(j) == 1)
 			return true;
+	}
 	return false;
+
 }
 
-void merge(SetGenerali a, SetGenerali b){
-	SetGenerali ciclo;
-	for(int el: a)
-		ciclo.insert(el);
-	for(int el: b)
-		ciclo.insert(el);
-	cicli.push_back(ciclo);
-}
