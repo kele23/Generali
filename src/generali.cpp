a2e9c83822510bed7ceae028fb5833788f0b0ba5
Michele Scala <Michele Scala>
2015-05-07 18:01:04 +0200

    37

--- src/generali.cpp
+++ src/generali.cpp
@@ -12,6 +12,7 @@
 
 //Comandato Da
 #define ROOT -1
+#define NONE -2
 
 //Colore BFS1
 #define WHITE	0
@@ -25,28 +26,39 @@
 */
 typedef std::unordered_set< int > SetNodi;
 
+typedef struct _LNodo{
+	int n;
+	struct _LNodo* next;
+}LNodo;
+
 /**
 * Item dell'array di Generali
 */
 struct Generale{ //Generale contenuto all'interno dell'Array
+	//Tarjan
 	int indice = -1;
 	int min_dist;
-	SetNodi predecessori;
-	SetNodi successori;
-	SetNodi sottoposti;
 	bool onStack = false;
 
-	int comandato = ROOT;
+	//Grafo
+	LNodo* predecessori = NULL;
+	LNodo* successori = NULL;
+
+	//Ciclo
+	int scc = NONE;
+
+	//Albero
+	int comandato = NONE;
 };
 
 std::vector<SetNodi> scc;
 int indice = 0;
 std::stack<int> s;
 Generale* grafo;
+int currentScc = 0;
 
 void tarjan(int nodo);
-bool nodoGiaPresente(int nodo, std::stack<int> st);
-bool sameScc(int i, int j);
+void rimuoviPerdenti(int);
 	
 int main(int argc, char* argv[]){
 
@@ -54,31 +66,49 @@ int main(int argc, char* argv[]){
 	
 	int V = 0;
 	int E = 0;
+	LNodo* current;
 
-	//LETTURA DEL GRAFO
+	
 
+
+
+
+	/**********************************************************
+	*****LETTURA********/
 	start = std::chrono::system_clock::now();
 	FILE *input = fopen("input.txt","r");
 	
 	fscanf(input,"%d %d",&V,&E);
 	grafo = new Generale[V];
 
-	int vinc,perd;
 	for(int I = 0; I < E; I++){
+		int vinc,perd;
 		fscanf(input,"%d %d",&vinc,&perd);
-		grafo[perd].predecessori.insert(vinc);
-		grafo[vinc].successori.insert(perd);
+		
+		LNodo* pv = new LNodo;
+			pv->n = vinc;
+			pv->next = grafo[perd].predecessori;
+			grafo[perd].predecessori = pv;
+
+		LNodo* vp = new LNodo;
+			vp->n = perd;
+			vp->next = grafo[vinc].successori;
+			grafo[vinc].successori = vp;
 	}
 
 	fclose(input);
 	end = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed_seconds = end-start;
-
 	std::cout << "Lettura: " << elapsed_seconds.count() << std::endl;
+	/**********************************************************
+	*****LETTURA********/
 
-	//ELABORAZIONE DEL RISULTATO
+
+
+
+	/**********************************************************
+	*****TARJAN********/
 	start = std::chrono::system_clock::now();
-	
 	//Identificazione SCC
 	for(int i=0; i<V; i++)
 		if(grafo[i].indice == -1)
@@ -86,59 +116,93 @@ int main(int argc, char* argv[]){
 	
 	end = std::chrono::system_clock::now();
 	elapsed_seconds = end-start;
+	std::cout << "Tarjan: " << elapsed_seconds.count() << std::endl;	
+	/**********************************************************
+	*****TARJAN********/
 
-	std::cout << "BFS: " << elapsed_seconds.count() << std::endl;	
 
-	//Costruzione dell'albero
-	start = std::chrono::system_clock::now();
-	std::stringstream ssCons;
-	std::stringstream ssSottoposti;
-	int numConsiglieri = 0;	
 
-	for(int i=0; i<V; i++) {
-		for(int s: grafo[i].successori){
-			if(!sameScc(i,s)){
-				bool ok = true;
-				for(int f: grafo[i].sottoposti)
-					if(sameScc(f,s))
-						ok = false;
-				if(ok){
-					grafo[i].sottoposti.insert(s);
-					grafo[s].comandato = i;
-					ssSottoposti << i << " " << s << std::endl;
+
+
+	/**********************************************************
+	*****SCC RIMOZIONE ARCHI********/
+	start = std::chrono::system_clock::now();	
+	for(int i = 0; i < V; i++) {
+		if(grafo[i].scc == NONE)
+			continue;
+
+		LNodo* pre = NULL;
+		current = grafo[i].successori;
+		while( current != NULL)  {
+			int succ = current->n;
+
+			bool modified = false;
+			//std::cout<< i << " scc: " << grafo[i].scc << " -- " << succ << " scc: " << grafo[succ].scc <<std::endl;
+			if(grafo[i].scc == grafo[succ].scc){
+				//std::cout << "       removed" << std::endl;
+				if(pre == NULL){
+					grafo[i].successori = current->next;
 				}
+				else{
+					pre->next = current->next;
+				}
+				modified = true;
 			}
+
+			if(!modified)
+				pre = current;
+			current = current->next;
 		}
 	}
 	end = std::chrono::system_clock::now();
 	elapsed_seconds = end-start;
-	std::cout << "Albero: " << elapsed_seconds.count() << std::endl;
+	std::cout << "Rimozione Archi: " << elapsed_seconds.count() << std::endl;
+	/**********************************************************
+	*****SCC RIMOZIONE ARCHI********/
+
 
 
 	int consiglieri = 0;
-	for(int i=0; i<V; i++) {
+	std::stringstream ssCons;
+	std::stringstream ssSottoposti;
+
+
+	/**********************************************************
+	*****CREAZIONE ALBERO********/
+	for(int i = 0; i < V; i++) {
+
+		current = grafo[i].successori;
+		while( current != NULL)  {
+			int succ = current->n;
+
+			grafo[succ].comandato = i;
+
+			current = current->next;
+		}
+	}
 		
+	for(int i = 0; i < V; i++) {
+
+		if(grafo[i].comandato == NONE)
+			grafo[i].comandato = ROOT;
+
 		if(grafo[i].comandato == ROOT){
 			consiglieri++;
 			ssCons << i << " ";
 		}
-
-		//Stampa sottoposti
-		/*std::cout << i << ": " << grafo[i].comandato <<" -> ";
-		for(int g: grafo[i].sottoposti){
-			std::cout << g << " ";
+		else{
+			ssSottoposti << grafo[i].comandato << " " << i << std::endl;
 		}
-		std::cout << std::endl;*/
 	}
-
-
+	/**********************************************************
+	*****CREAZIONE ALBERO********/
 
 	start = std::chrono::system_clock::now();
 	FILE* output = fopen("output.txt","w");
 
 	fprintf(output,"%d\n",consiglieri);
 	fprintf(output,"%s\n",ssCons.str().c_str());
-	fprintf(output,"%s\n",ssSottoposti.str().c_str());
+	fprintf(output,"%s",ssSottoposti.str().c_str());
 	
 	fclose(output);
 	end = std::chrono::system_clock::now();
@@ -157,13 +221,18 @@ void tarjan(int nodo) {
 	s.push(nodo);
 	grafo[nodo].onStack = true;
 	int tmp;
-	for(int succ: grafo[nodo].successori) {
+	LNodo* current = grafo[nodo].successori;
+	while( current != NULL)  {
+		int succ = current->n;
+
 		if(grafo[succ].indice == -1) {
 			tarjan(succ);
 			grafo[nodo].min_dist = std::min(grafo[nodo].min_dist, grafo[succ].min_dist);
 		}
 		else if(grafo[nodo].onStack) 
 			grafo[nodo].min_dist = std::min(grafo[nodo].min_dist, grafo[succ].indice);
+
+		current = current->next;
 	}
 	if(grafo[nodo].min_dist == grafo[nodo].indice) {
 		SetNodi setN;
@@ -172,20 +241,12 @@ void tarjan(int nodo) {
 			tmp = s.top();
 			grafo[tmp].onStack = false;
 			setN.insert(tmp);
+			grafo[tmp].scc = currentScc;
 			s.pop();
 		} while (tmp != nodo);
 
 		scc.push_back(setN);
+		currentScc++;
 	}
-}
-
-bool sameScc(int i, int j){
-
-	for(SetNodi set: scc){
-		if(set.count(i) == 1 && set.count(j) == 1)
-			return true;
-	}
-	return false;
-
 }
 
